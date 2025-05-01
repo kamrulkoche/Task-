208e334b2dec8f8edeceb844a6f93df7f5703bcc
kamrulkoche <kamrulkoche@gmail.com>
2025-05-01 15:16:58 +0600

    login and signUp page

--- src/components/SignUpFrom/SignUpFrom.tsx
+++ src/components/SignUpFrom/SignUpFrom.tsx
@@ -6,7 +6,7 @@ const SignUpFrom = () => {
     return (
         <div className="min-h-screen bg-[#f0e6cc] flex items-center px-4 -mt-20 justify-center gap-16">
             <div className="w-full lg:w-1/2  p-8 ">
-                <h2 className="text-3xl font-semibold text-center mb-8 text-gray-800">Log In</h2>
+                <h2 className="text-3xl font-semibold text-center mb-8 text-gray-800">Sign Up</h2>
 
 
                 <div className="mb-4">
